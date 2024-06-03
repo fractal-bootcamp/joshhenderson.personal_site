3d31e8ef9d4c9bd752acea608c08929c0bde7222
Joshua Henderson-Jardim <hendersonjardimj@gmail.com>
2024-06-03 12:09:02 -0400

    makes for a first pull request

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Josh's Site" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,12 +1,14 @@
 export default function About() {
     return (
-        <div> 
+        <><div> 
             <h2 className="">About</h2>
             <p>
                 Hey, I'm Josh. I'm learn to code at Fractal. I love playing soccer and listening to my hifi system 
             </p>
-            <img src="/public/IMG_3977.jpeg" className="max-w-md"/>
         </div>
+        <div className="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
+            <img src="/public/IMG_3977.jpeg" className="max-w-md"/>
+        </div></>
     
     )
 };
