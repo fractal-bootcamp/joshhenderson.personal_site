080259d87a2bba4be932c8e6d85d282879dac897
Joshua Henderson-Jardim <hendersonjardimj@gmail.com>
2024-06-03 11:41:41 -0400

    first commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,35 +7,22 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const Header = () => {
+  return(<header>
+    <nav>
+      <ul>
+        <li><a href="./about">about</a></li>
+        <li><a href="https://github.com/Hendersonajardimj">github</a></li>
+        <li><a href="./about">Portfolio</a></li>
+      </ul>
+    </nav>
+  </header>)
+  };
+
+
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <Header/>
   );
-}
+};
---
+++ app/routes/about.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+    return (
+        <div> 
+            <h2 className="">About</h2>
+            <p>
+                Hey, I'm Josh. I'm learn to code at Fractal. I love playing soccer and listening to my hifi system 
+            </p>
+            <img src="/public/IMG_3977.jpeg" className="max-w-md"/>
+        </div>
+    
+    )
+};
+
